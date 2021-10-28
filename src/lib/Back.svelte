d9313e9bec8b93fb8cba80a05f819bd0018db91a
Odilf <odysseas.maheras@gmail.com>
2021-10-28 16:48:24 +0200

    made back sticky

--- src/lib/Back.svelte
+++ src/lib/Back.svelte
@@ -2,10 +2,26 @@
 	// import { mdiChevronLeft } from '@mdi/js'
 </script>
 
-<button on:click={() => window.history.back()}> Back </button>
+<header>
+	<button on:click={() => window.history.back()}> Back </button>
+</header>
 
 <style>
+	header {
+		position: sticky;
+		top: 0.5rem;
+		/* background-color: var(--secondary); */
+		border-radius: 1em;
+		height: 3em;
+		width: 100%;
+
+		opacity: 80%;
+
+		display: grid;
+		/* place-items: center; */
+	}
 	button {
+		background-color: var(--primary);
 		font-size: 1rem;
 	}
 </style>
