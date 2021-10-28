4ce92b7faa459347ffd3d72eb64ff88f663ff061
Odilf <odysseas.maheras@gmail.com>
2021-10-28 16:51:44 +0200

    added the menacing letter

--- src/routes/legal.svelte
+++ src/routes/legal.svelte
@@ -16,7 +16,38 @@
 	<br>
 	<p>
 		May God have mercy on you. 
+	<br>
+	<br>
+	<br>
+
+	<p>
+		A very menacing letter is attached at the end of this document. Viewer discretion is advised. 
+	</p>
+
+	<MenacingLetter>
+		<div>
+			I just saw your new profile picture and I wanted to say that a) how cool a cat and b) haha fat but c) is not Maggie and as a representative of the judicial department of the official club of fans and worshipers of Maggie (FAM) I am afraid that several lawsuits will arrive over the following days for reason of treachery and also as representative of the <a href='/executive'>executive department of FAM </a> the <a href='/guillotine'> guillotine </a> is ready for any day next week <br>
+			<br>
+
+			Thanks for your understanding, <br>
+			sucks to be you
+		</div>	
+		<div slot=translation>
+			acabo de ver tu nueva foto de perfil y queria decir que a) que guay un gato y b) jaja gordo pero c) no es maggie y como representante del departamento judicial del club oficial de fans y adoradores de Maggie (FAM) me temo que llegaran varias lawsuits los proximos dias a motivo de treachery y tambien como representante  del departamento ejecutivo de FAM la guillotina esta preparada para cualquier dia de la semana que viene <br>
+			<br>
+			
+			gracias por su entendimiento, <br>
+			sucks to be you
+		</div>
+	</MenacingLetter>
+
+	<p>
+		This department is run by sir Odilf Gamer, the most menacing version of Odilf. 
 	</p>
 </body>
 
-<MenacingLetter> Wow im so menacing</MenacingLetter>
+<style>
+	a {
+		color: var(--accent);
+	}
+</style>
