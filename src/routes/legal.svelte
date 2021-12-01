f3192616febe5ae860e48642269f95ca9b9b4bb9
Odilf <odysseas.maheras@gmail.com>
2021-12-01 13:07:36 +0100

    fixed typo

--- src/routes/legal.svelte
+++ src/routes/legal.svelte
@@ -12,7 +12,7 @@
 
 <body>
 	<p>
-		The legal department is menacing. Very, <i> very </i> menacing. We advise to avoid it at all costs. The legal department is relentless and will not hesitate to lawsuit anyone out of existance, if they shall dare challenge the legal department. These oh so naive people that attempted the impossible are naught but the remains of what has become a missbegoten lunacy. Lunacy is the word, gentlemen, lunacy to think you are above the all-mighty legal department. 
+		The legal department is menacing. Very, <i> very </i> menacing. We advise to avoid it at all costs. The legal department is relentless and will not hesitate to sue anyone out of existance, if they shall dare challenge the legal department. These oh so naive people that attempted the impossible are naught but the remains of what has become a missbegoten lunacy. Lunacy is the word, gentlemen, lunacy to think you are above the all-mighty legal department. 
 	<br>
 	<p>
 		May God have mercy on you. 
