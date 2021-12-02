84fc34905ca8cce9bc85bf4b06f37d48518a504b
Odilf <odysseas.maheras@gmail.com>
2021-12-02 09:45:01 +0100

    Added text to staff

--- src/lib/Staff.svelte
+++ src/lib/Staff.svelte
@@ -1,34 +1,43 @@
 <script lang="ts">
 	import Card from "./Card.svelte";
+	import { staff } from '$lib/photos'
 </script>
 
 <section>
-	<Card color='#3333FF' photo_src='https://lh3.googleusercontent.com/pw/AM-JKLWs9kpvtFFt1OAG23j7_jhZMN3eHsZCqsVCiXOanml8ftyKJ2CCqgdjTnZCspCdO9XmOoUmNVLv51coOql04EomX4AcCvuqpUO7UsCo_IOfI9__9Xh3i0EJNx9K56tpUZAHO147Fp8zSziyGBymb9wpiA=w735-h980-no?authuser=0'>
+	<Card color='#3333FF' photo={staff.clara}>
 		<span slot='heading'>
 			Clara
 		</span>
-	
 		<p>
-			Being the head of the private paparazzi department, Clara is an expert at loving Maggie. She's a loyal devotee that has deep knoledge about how Maggie is best portrayed in print.
+			Clara is considered the mother of Maggie. Tactical repositioning of the headquarters have made that Clara can always be in extremely near proximity to Maggie, since day-long care is essential to her wellbeing.
 		</p>
 		<p>
-			Furthermore, Clara is considered the mother of Maggie. Hugs, cudles and kisses with Maggie are a common occurence.
-		</p>
-		<p>
-			Tactical repositioning of the headquarters have made that Clara can always be in extremely near proximity to Maggie, since day-long care is essential to her wellbeing.
+			Furthermore, being the head of the <a href='private-paparazzi'> private paparazzi </a> department, Clara is an expert at loving Maggie. She's a loyal devotee that has deep knoledge about how Maggie is best portrayed in print.
 		</p>
 	</Card>
 	
-	<Card color='#32BC00' photo_src='https://lh3.googleusercontent.com/59-HuvwCUOv36-qE_yrkd_429OHmQGPib0n7_wxbgevm__wGxvlBkcciJTv8INzDKwTQ78e9K5fdpA3NtCzPg9-ll6dXCgo7w865Se1_ZaofNrA1_XdtEfuMOLAuBNkgm4TJ-I6nBd5O7OZ5fVqfXsKhGdfDOy612Mhv2Nl4j6y4ADdeAZZnLDWrVlgvY_RBwELSLRvYYYLVfpOA9CBz3Htfkz1Ycw5g1mUWsWETXhAjR_iiajnyJ-28McX3tPYVeBgYCKFqruzfLy6FjA2n_EZXeY-40ffiZ59PzSgpG-sKwWG7CO41ry3tIiclsbcWMl_tR25BUQD9rd0xU4mYvdDcVxID7yNQuknoDDfEMpn6kr6bSV5qqD9K5SUqkcxM7ttqb8v1vOzzdksEDF7XZhupkcYGDVhTochHYKj4TTjIJp7PXdNmN6Twd2KGCYq2QOKcNyOtL54R3bfwBbwejnRKj_SDcUBHPwEGUnvzW4E_g7aIm2cFRMcXIsNFez5RfWFYXumvM0WsZ0RO7hmeZqtLEhBDr9ugt6CjPgcimtLjRqxQgawJU0KABh1ni-LXsOLclkTRVdxILBWdCQ87_kyhC5JUfKuqKS-D-We6h7pt3rWjitVcTRXpMOemxAm1j-a61XvA26RXmHUOthjibQH0dy-OB1hSOHKQBsEAXKeDo4PE8_8I0mito06jXUFX4mtfzWQ3ZaJaCQDR6r8EwYrlVQ=w750-h422-no?authuser=0'>
+	<Card color='#32BC00' photo={staff.javi}>
 		<span slot='heading'>
 			Javi
 		</span>
+		<p>
+			Javi is the father and owner of Maggie. He has the duty of feeding her, and cleaning up her vomit. 
+		</p>
+		<p>
+			Is there something a man can do? Then Javi does. That's almost his motto. From unicycling to doing backflips in the air, the range of abilities to Javi can often be breathtaking.  
+		</p>
 	</Card>
 	
-	<Card color='red' photo_src='https://lh3.googleusercontent.com/_s4CGw6ZfKoBSJ6H_3Nqfqx9VWJbp7QNIjaJIyixoNjs3A70rGAs_AJcZ53s-FN8Iuga_CleJDzZaTpA0yJE6qzcvB-uduCSKuL64HtA81pK_cQSn_yyZY-YCpxmIEETAy693uxkaKd0_0i-mtnLOYs4c4p_WqMDvT8E9k4ZpWzhMnEf_LDm6S20sBu5Ud9yT449Qy4FhNGJ6US5Ast0NAgTkhFKSBkkKmEJi9xytx_vPn6CWvSF3jLJNBAWTI7RRM1GXqSm3wL9fA2Ta4p7Z_jUXEqLx5VccMLJSe9cOZBycPKGooijTGR8w9jyHZHyKGgr4hrdpfVpeQbQwD8_lK1TiwfMZ95LeD8WcopOW3qd1oegDWS2g0LNV6A39mWwlUhg12oOSXDEPfVD9ea4B7DK8wGLRNwaTCMjKoz7udjxlqSnJiTZZzsRHGTzArwCJRNthEkq-R5B3mDrcjcww4itcWFVAZ-0cCmUo-uRC5b_TlKKqOIiE1jN4LSPi3vFZTwiYboyFiOrjCtPSICZdshBk-ylSkN79SRlpDw4Lx-HLgbR2GeO9WFlm3-G3i5oUMd_JKqkWmXvQTZhgnvmnNpne-nCt86IA9OtiXtMBEHcMGItw528gGoMidJV9cgC8fZbVslNOy3ffCXEnQhDuSRNKuXz1zHf4aAKoPPVf37cTGPMgc249M1XwWYsfeaG_cBa6bHPDEG99msKiAurj4E77g=w139-h204-no?authuser=0'>
+	<Card color='#D62F09' photo={staff.ody}>
 		<span slot='heading'>
 			Ody
 		</span>
+		<p>
+			Ody is in charge of all things technical related to the conservation and elevation of the image of Maggie.
+		</p>
+		<p>
+			Ody operates the <a href='/executive'> executive department </a> as Odilf, and the <a href="/legal"> legal department </a> as OdilfGamer, the most menacing version of Odilf. 
+		</p>
 	</Card>
 </section>
 
@@ -38,5 +47,13 @@
 		flex-wrap: wrap;
 		justify-content: space-evenly;
 		width: 100%;
+	}
+	a {
+		color: var(--primary);
+		text-decoration: underline;
+		transition: all 0.2s;
+	}
+	a:hover {
+		text-shadow: 1px 1px 1px black;
 	}
 </style>
